0a310d63c087166d145990622f4ca6f75d621d2e
Kimberly Oleiro <koleiro@gmail.com>
2016-04-17 16:28:49 -0400

    Played around with CSS

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
 
   resources :sessions, only: %i(new create destroy)
 
-  resources :reservations, only: %i(new create destroy show)
+
 
   resources :users, only: %i(new create)
 
-  resources :restaurants, except: %i(destroy update edit)
+  resources :restaurants, except: %i(destroy update edit) do
+    resources :reservations, only: %i(new create destroy show)
+  end
+
+
   # get 'categories/create'
   #
   # get 'categories/new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Restaurant.create!(
   :price => 3,
   :address => '123 sesame',
   :phone_number => '123-456-7890',
-  :category_id => 8,
+  :category_id => 2,
   :capacity => 55,
   :open_hours => '11:00',
   :close_hours => '20:00'
