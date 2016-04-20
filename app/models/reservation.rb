57c055cf675800b0323e07717dda6e1b19cac2aa
Dennis Choi <dennis.choi7@gmail.com>
2016-04-20 16:35:49 -0400

    change the validations to include and if statement for presence true of resdatetime and seats

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,15 +4,17 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
 
   validates :res_date_time, :seats, presence: true
-  validate :not_full
+  validate :not_full #, if: 'res_date_time.present? && seats.present?'
 
   private
 
   def not_full
+    if res_date_time.present? && seats.present?
       reserved_seats = restaurant.reservations.where(:res_date_time => res_date_time.beginning_of_hour...res_date_time.end_of_hour).sum(:seats)
       if reserved_seats + seats > self.restaurant.capacity
         errors.add(:seats, "No mo seats")
       end
+    end
   end
 
 end
