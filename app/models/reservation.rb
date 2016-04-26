a506dc9249b39521da3cd331784cfe6f30a43779
Dennis Choi <dennis.choi7@gmail.com>
2016-04-25 23:38:56 -0400

    Started future reservation method in reservation.rb.  Added error message to reservations partial form.  Changed restaurant.rb where there is 2 of the same has_many values and put in class_name

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,12 +9,17 @@ class Reservation < ActiveRecord::Base
   private
 
   def not_full
-    if res_date_time.present? && seats.present?
+    if res_date_time.present? && seats.present? # && restaurant.present?
       reserved_seats = restaurant.reservations.where(:res_date_time => res_date_time.beginning_of_hour...res_date_time.end_of_hour).sum(:seats)
       if reserved_seats + seats > self.restaurant.capacity
         errors.add(:seats, "No mo seats")
       end
     end
   end
+  #
+  # def
+  #   res_date_time < Date.today
+  #   errors[:base] << 'message'
+  # end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,8 @@ class Restaurant < ActiveRecord::Base
   belongs_to :category
 
   has_many :reviews
-  has_many :users, through: :reviews
+  has_many :reviewing_users, through: :reviews, class_name: 'users'
   has_many :reservations
-  has_many :users, through: :reservations
+  has_many :reserving_users, through: :reservations, class_name: 'users'
 
 end
